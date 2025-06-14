b5cd9a396a1b806fc4ceb8fe7909a61bbf5be23e
Subashree-selvaraj <subashreevjc@gmail.com>
2025-06-14 16:53:38 +0530

    updation of render.yaml file

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://bug-tracker-frontend.onrender.com', 'http://localhost:3000']
+    ? ['https://bug-tracker.onrender.com', 'http://localhost:3000']
     : 'http://localhost:3000',
   credentials: true
 }));
